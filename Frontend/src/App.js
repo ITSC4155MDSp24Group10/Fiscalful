a2f21d34d0e9ab0d3c7f7eedb53d8f694ebd79c4
Trevor Richardson <Tjrich336@gmail.com>
2024-02-21 10:43:10 -0500

    Feat: Create assets and components folders along with all page folders and templet files with react router configured in App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Landing from "./Components/Landing/Landing";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <main className="main">
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+
+                </Routes>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
---
+++ Frontend/src/Components/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './landing.css';
+
+const Landing = () => {
+  return (
+    <section className='landing section' id='landing'>
+        <div>
+            <h1>Welcome to our website!</h1>
+            <p>This is the landing page.</p>
+        </div>
+    </section>
+  );
+};
+
+export default Landing;
