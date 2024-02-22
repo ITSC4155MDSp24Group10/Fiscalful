f849f012fdd80b8c54469db71f0b62f97e423780
Trevor Richardson <Tjrich336@gmail.com>
2024-02-22 13:42:46 -0500

    Feat: Implemented Header links and routing to navigate through pages.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
-import Landing from "./Components/Landing/Landing";
-
+import Header from "./Components/Header/Header";
+import Landing from "./Components/Landing/Landing"; 
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Login from "./Components/Login/Login";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
     return (
-        <Router>
-            <main className="main">
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-
-                </Routes>
-            </main>
-        </Router>
+        <>
+            <Router>
+                <Header />
+                    <main className="main">
+                        <Routes>
+                            <Route path="/" element={<Landing />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Routes>
+                    </main>
+            </Router>
+        </>
     );
 }
 
