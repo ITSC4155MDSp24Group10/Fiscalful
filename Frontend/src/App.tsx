ccfb9706c6ab22b11f6be69c5413dd4300399767
Clubin1 <justin.03.tubay@gmail.com>
2024-03-03 20:33:19 -0500

    s

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,107 +1,32 @@
-import React, { useEffect, useContext, useCallback } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import "./App.css"; Maybe not needed idk if we need it we can grab from the old frontend folder
+import Header from "./Components/Header/Header";
+import Landing from "./Components/Landing/Landing"; 
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
-import Header from "./Components/Headers";
-import Products from "./Components/ProductTypes/Products";
-import Items from "./Components/ProductTypes/Items";
-import Context from "./Context";
-
-import styles from "./App.module.scss";
-
-const App = () => {
-  const { linkSuccess, isItemAccess, isPaymentInitiation, dispatch } = useContext(Context);
-
-  const getInfo = useCallback(async () => {
-    const response = await fetch("/api/info", { method: "POST" });
-    if (!response.ok) {
-      dispatch({ type: "SET_STATE", state: { backend: false } });
-      return { paymentInitiation: false };
-    }
-    const data = await response.json();
-    const paymentInitiation: boolean = data.products.includes(
-      "payment_initiation"
+function App() {
+    return (
+        <>
+            <Router>
+                <Header />
+                    <main className="main">
+                        <Routes>
+                            <Route path="/" element={<Landing />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Routes>
+                    </main>
+            </Router>
+        </>
     );
-    dispatch({
-      type: "SET_STATE",
-      state: {
-        products: data.products,
-        isPaymentInitiation: paymentInitiation,
-      },
-    });
-    return { paymentInitiation };
-  }, [dispatch]);
-
-  const generateToken = useCallback(
-    async (isPaymentInitiation) => {
-      // Link tokens for 'payment_initiation' use a different creation flow in your backend.
-      const path = isPaymentInitiation
-        ? "/api/create_link_token_for_payment"
-        : "/api/create_link_token";
-      const response = await fetch(path, {
-        method: "POST",
-      });
-      if (!response.ok) {
-        dispatch({ type: "SET_STATE", state: { linkToken: null } });
-        return;
-      }
-      const data = await response.json();
-      if (data) {
-        if (data.error != null) {
-          dispatch({
-            type: "SET_STATE",
-            state: {
-              linkToken: null,
-              linkTokenError: data.error,
-            },
-          });
-          return;
-        }
-        dispatch({ type: "SET_STATE", state: { linkToken: data.link_token } });
-      }
-      // Save the link_token to be used later in the Oauth flow.
-      localStorage.setItem("link_token", data.link_token);
-    },
-    [dispatch]
-  );
-
-  useEffect(() => {
-    const init = async () => {
-      const { paymentInitiation } = await getInfo(); // used to determine which path to take when generating token
-      // do not generate a new token for OAuth redirect; instead
-      // setLinkToken from localStorage
-      if (window.location.href.includes("?oauth_state_id=")) {
-        dispatch({
-          type: "SET_STATE",
-          state: {
-            linkToken: localStorage.getItem("link_token"),
-          },
-        });
-        return;
-      }
-      generateToken(paymentInitiation);
-    };
-    init();
-  }, [dispatch, generateToken, getInfo]);
-
-  return (
-    <div className={styles.App}>
-      <div className={styles.container}>
-        <Header />
-        {linkSuccess && (
-          <>
-            {isPaymentInitiation && (
-              <Products />
-            )}
-            {isItemAccess && (
-              <>
-                <Products />
-                <Items />
-              </>
-            )}
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
+}
 
 export default App;
