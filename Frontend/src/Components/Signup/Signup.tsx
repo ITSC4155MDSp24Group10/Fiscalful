5a0e879e2d6ef3638cfdeacf2a220f271d07dee9
Trevor Richardson <Tjrich336@gmail.com>
2024-03-11 19:26:27 -0400

    Fix: Updated sign up password and formatting.

--- Frontend/src/Components/Signup/Signup.tsx
+++ Frontend/src/Components/Signup/Signup.tsx
@@ -5,15 +5,17 @@ import React, { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './signup.css';
 
+
 function Signup() {
-  const [Name, setName] = useState('');
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error] = useState('');
   const navigate = useNavigate();
 
   const Create = (e: FormEvent) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, Email, Password)
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
         navigate('/dashboard');
@@ -25,39 +27,47 @@ function Signup() {
 
   return (
     <section className="signup section" id="signup">
-      <h1 className="signup__title">Sign Up</h1>
-      <span className="signup__subtitle">Register An Account</span>
+      <div className='signup-container'>
+        <h1 className="signup__title">Sign Up</h1>
+        <span className="signup__subtitle">Register An Account</span>
 
-      <form onSubmit={Create}>
-        <h3 className="signup">Create An Account</h3>
-        <input
-          className="name"
-          type="text"
-          placeholder="Enter Your Name"
-          value={Name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          className="email"
-          type="email"
-          placeholder="Enter Your Email"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="password"
-          type="password"
-          placeholder="Enter Your Password"
-          value={Password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="signup__button" type="submit">
-          Sign Up
-        </button>
-      </form>
-      <Link to="/login" className="login-toggle">
-        <i></i> Already Have An Account? Login!
-      </Link>
+        <div className='form-container'>
+          <h2 className="signup">Create An Account</h2>
+          <form onSubmit={Create}>
+            <input
+              className="name"
+              type="text"
+              placeholder="Enter Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              className="email"
+              type="email"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div className="password-container">
+              <input
+                className="password"
+                type='text'
+                placeholder="Enter Your Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {error && <p className="error">{error}</p>}
+            </div>
+            <button className="signup__button" type="submit">
+              Sign Up
+            </button>
+          </form>
+          
+          <Link to="/login" className="login-toggle">
+            <i></i> Already Have An Account? Login!
+          </Link>
+        </div>
+      </div>
     </section>
   );
 }
